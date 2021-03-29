dde63b1d3b94d668ab194c90000ca2083cc5228b
gilvan.muniz <camisetaecia@live.com>
2021-03-29 16:00:44 -0300

    consumindo api mokada

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,15 +1,32 @@
+import dados from '.';
 <template>
-  <div>
+  <div class="principal">
+    <div class="principal">
       <h1>Products</h1>
+      <div v-for="dado in dados" :key="dado.id">
+         <ul>
+           <li>
+             {{ dado.id }} - {{ dado.name }} - {{ dado.amount }} - {{ dado.price }}
+           </li>
+         </ul>
+      </div>
+      <hr>
+      <div class="mt-4" v-for="dado in dados" :key="dado.id">
+        <div v-if="dado.category.name == 'Papelaria'">{{ dado.name }} - {{ dado.price }}</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import dados from './../data/data.json'
 export default {
-
-}
+  data(){
+    return {
+      dados:dados
+    }
+  }
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
