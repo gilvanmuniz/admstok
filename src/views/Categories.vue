73ea708b3ac3ebce2375d5808e17ea50ead5eb67
gilvan.muniz <camisetaecia@live.com>
2021-04-03 14:55:38 -0300

    Consumindo dados das categorias pela api spring

--- src/views/Categories.vue
+++ src/views/Categories.vue
@@ -1,15 +1,53 @@
 <template>
   <div>
+    <div class="categories">
       <h1>Categories</h1>
+      <div class="list" v-for="category in categories" :key="category.id">
+        <div class="dados">{{ category.id }}</div>
+        <div class="dados">{{ category.name }}</div>        
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-
+  data(){
+    return {
+      categories:{}
+    }
+  },
+  methods:{
+    getCategories(){
+      axios.get('http://localhost:8081/categories').then(resp => {
+        this.categories = resp.data
+      })
+    }
+  },
+  created(){
+    this.getCategories()
+  }
 }
-</script>
 
-<style>
+</script>
+<style lang="scss" scoped>
+
+.categories{  
+  align-items: center;
+  text-align: center;
+  .list{
+    width: 50%;
+    margin-left: 22.5%;
+    align-items: center;
+    padding: 1%;
+    display: flex;
+    justify-content: center;
+    border: 1px solid grey;
+    .dados{
+      padding: 0.7%;
+    }
+  }
+}
 
 </style>
