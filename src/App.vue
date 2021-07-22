51c3a72cbf0f4436f13eab048f0c83a7fd534259
gilvanmuniz <camisetaecia@live.com>
2021-07-22 15:30:36 -0300

    created colors.scss e menuLogin e Login.vue

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,30 @@
 <template>
   <div id="nav">
+    <div class="login">
+      <div class="buttons">
+        <router-link to="/login">
+          <button class="btn btn-primary btn-sm">Login</button>
+        </router-link>
+        <router-link class="link ml-3 mt-2" to="#">
+          <h6>Cadastro</h6>
+        </router-link>
+      </div>
+    </div>
     <Menu />
-    <hr>
+    <hr />
   </div>
-  <router-view/>
+  <router-view />
 </template>
 <script>
-import Menu from './components/Menu'
+import Menu from "./components/Menu";
 export default {
-  components:{
-    Menu
-  }
-}
+  components: {
+    Menu,
+  },
+};
 </script>
 <style lang="scss">
+@import 'scss/colors.scss';
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -23,17 +34,17 @@ export default {
 }
 
 #nav {
-  padding: 30px;
-
-  hr{
-    border-bottom: 1.4px solid #2c3e50;
-  }
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+  .login {
+    width: 100%;
+    background-color: $light;
+    .buttons {
+      display: flex;
+      justify-content:flex-end;
+      align-items: center;
+      margin-right: 4%;
+      .link{
+        text-decoration: none;
+      }
     }
   }
 }
